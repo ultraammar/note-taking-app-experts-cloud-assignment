17b8a4d9b2ebfad05918e4dd5cb53e6337d9f3c1
Ammar Waheed <ultraammar@gmail.com>
2024-08-02 01:37:34 +0500

    testing with hardcoded creds
    will remove later

--- backend/database/db_connect.js
+++ backend/database/db_connect.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-    host: process.env.mysql_host,
-    port: process.env.mysql_port,
-    user: process.env.mysql_user,
-    password: process.env.mysql_password,
-    database: process.env.mysql_database, 
+    host: "mysql-3be8f42a-ultraammar-09ae.e.aivencloud.com",
+    port: "12512",
+    user: "avnadmin",
+    password: "AVNS_lsVS1ASx4RRa6fKIQst",
+    database: "defaultdb", 
 });
 
 module.exports = db;
