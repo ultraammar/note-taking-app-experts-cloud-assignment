e6d5da51cce6a0226095c145145f71749a8830d1
ultraammar <ultraammar@gmail.com>
2024-08-02 00:49:02 +0500

    commenting express cors config

--- backend/server.js
+++ backend/server.js
@@ -11,14 +11,14 @@ const notesController = require('./controllers/notesController');
 const app = express();
 
 //configure express app, for e.g to use json or other stuff like cors
-app.use(cors({
-  //origin: 'https://note-taking-app-experts-cloud-assignment.vercel.app',
-  origin: "https://note-taking-app-experts-cloud-assignment.vercel.app",
-  //origin: process.env.NODE_ENV === 'production' ? 'https://note-taking-app-experts-cloud-assignment.vercel.app' : true,
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  withCredentials: true,
+// app.use(cors({
+//   //origin: 'https://note-taking-app-experts-cloud-assignment.vercel.app',
+//   origin: "https://note-taking-app-experts-cloud-assignment.vercel.app",
+//   //origin: process.env.NODE_ENV === 'production' ? 'https://note-taking-app-experts-cloud-assignment.vercel.app' : true,
+//   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+//   withCredentials: true,
   
-}));
+// }));
 app.use(express.json());
 
 
